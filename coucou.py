8436574d72e80d01676a89b0cf4bf0f1031b02cc
olyju1966 <86019321+olyju1966@users.noreply.github.com>
2021-08-02 17:30:52 +0200

    Create coucou.py

---
+++ coucou.py
@@ -0,0 +1,53 @@
+import json # For interacting with json
+from pathlib import Path # For paths
+import platform # For stats
+import logging
+import json
+import asyncio
+import discord
+import inspect
+import random
+import datetime
+import youtube_dl 
+import time
+import os
+from discord.ext.commands import has_permissions, MissingPermissions, BotMissingPermissions
+
+
+from discord.ext import commands
+from discord.utils import get
+
+client = commands.Bot(
+    command_prefix='/',
+    description="Bot de olyju1966",
+    owner_ids=(551134188695191563, 611886333232676889, 796905945229033522, 681974688062242850)
+)
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Game(name="faire la commande /help"))
+    print(f"Bot prêt, {len(client.guilds)} serveurs")
+
+@client.command()
+async def coucou(ctx):
+    """ renvoit coucou """
+    
+    await ctx.send("coucou")
+    
+    def read_json(filename):
+    with open(f"{filename}.json", "r") as f:
+        data = json.load(f)
+    return data
+
+def write_json(data, filename):
+    with open(f"{filename}.json", "w") as f:
+        json.dump(data, f, indent=4)
+
+def get_token() -> Optional[str]:
+    if client.is_ready():
+        return
+
+    with open("token") as f:
+        return f.read()
+    
+client.run(get_token())
